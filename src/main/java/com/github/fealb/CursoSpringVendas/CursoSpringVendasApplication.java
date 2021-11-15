0185a94804b9863426114816678d66214ab49bfb
Felipe <felipe.r.albuquerque@gmail.com>
2021-11-15 17:14:54 -0300

    Projeto inicial

---
+++ src/main/java/com/github/fealb/CursoSpringVendas/CursoSpringVendasApplication.java
@@ -0,0 +1,13 @@
+package com.github.fealb.CursoSpringVendas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CursoSpringVendasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CursoSpringVendasApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/github/fealb/CursoSpringVendas/CursoSpringVendasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.fealb.CursoSpringVendas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CursoSpringVendasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
